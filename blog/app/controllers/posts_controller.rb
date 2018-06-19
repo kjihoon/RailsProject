54510e5150d7c21fce745549f2db0a96bc5098f1
kjihoon <kjihoon0914@naer.com>
2018-06-19 15:05:51 +0900

    CRUD

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -9,17 +9,23 @@ class PostsController < ApplicationController
   def create
     # 두 가지 방법
     #1.
-    Post.create(title: params[:title], body: params[:body] )
+    post =Post.create(title: params[:title], body: params[:body] )
     #1.2
     #Post.create(:title => params[:title], :body => parmas[:body] )
     #2.
     #post = Post.new
     #post.title =  params[:title]
     #post.body = parmas[:body]
+    redirect_to "/posts/#{post.id}"
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    post =Post.find(params[:id])
+    post.destroy
+    redirect_to "/"
+  end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'posts/create'
   get 'posts/:id' => 'posts#show'
-
+  get 'posts/:id/destroy' => 'posts#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
