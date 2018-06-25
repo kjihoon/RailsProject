5f48cf06a7a873fe862a43e28a008ecc7fb3d2a1
aeriSon <sonsoo1224@gmail.com>
2018-06-25 16:50:45 +0900

    0625

---
+++ fake_insta/app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  bafore_action :authenticate_user!, except: :index
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/"
+  end
+
+  def show
+    @comments = @post.comments
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to "posts/#{@post.id}"
+  end
+
+  def destroy
+    @post.destroy
+    redirct_to "/"
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.permit(:title, :content)
+  end
+end
