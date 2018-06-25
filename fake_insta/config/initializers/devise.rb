8639273724e808a945e80f270ce9cb2eb1631a9d
kjihoon0914 <kjihoon0914@naver.com>
2018-06-25 17:49:01 +0900

    devise

--- fake_insta/config/initializers/devise.rb
+++ fake_insta/config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '172a04d77306fbb76024cbde9c1a5f51789466cef4cfa908cc6661d47ece568d79f1fe8d997027853962f6106cff912acea0fde2910a44ebc264f04a0ae8448e'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
--- fake_insta/db/schema.rb
+++ fake_insta/db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180625071504) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
