78f2c7c43194635568fd59c39f664cfe08085e3b
kjihoon <kjihoon0914@naver.com>
2018-06-26 12:00:57 +0900

    input faker data

--- fake_insta/db/seeds.rb
+++ fake_insta/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+Post.create(
+  title: Faker::Pokemon.name,
+  content: Faker::OnePiece
+)
+end
