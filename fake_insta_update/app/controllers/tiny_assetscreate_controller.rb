c453d9608ff92f2e827815ebe5e57203bd952343
kjihoon <kjihoon0914@naver.com>
2018-07-09 16:33:41 +0900

    0709

--- fake_insta_update/app/controllers/tiny_assetscreate_controller.rb
+++ fake_insta_update/app/controllers/tiny_assetscreate_controller.rb
@@ -16,7 +16,7 @@ class TinyAssetscreateController < ApplicationController
   private
   def upload_image(file)
     s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
-    obj = s3.bucket(ENV['S3S3_BUCKET_NAME']).object('/posts/content'+filename(file))
+    obj = s3.bucket(ENV['S3S3_BUCKET_NAME']).object('posts/content'+filename(file))
     obj.upload_file(file.tempfile, {acl: 'public-read'})
     obj.public_url.to_s
   end
